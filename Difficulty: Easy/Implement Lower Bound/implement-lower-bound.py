7b7917fbd281d896e14d9727249700de80c39593
Rajan Kumar <101178067+rajank404@users.noreply.github.com>
2026-03-20 09:54:20 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Implement Lower Bound/implement-lower-bound.py
+++ Difficulty: Easy/Implement Lower Bound/implement-lower-bound.py
@@ -1,10 +1,27 @@
 class Solution:
     def lowerBound(self, arr, target):
         # code here
-        for i in range(len(arr) -1):
-            if arr[i] >= target:
-                return i
-            elif arr[i] <= target and arr[i+1] >= target:
-                return i+1
-        return len(arr)
         
+        # m--- 1
+        
+        # for i in range(len(arr) -1):
+        #     if arr[i] >= target:
+        #         return i
+        #     elif arr[i] <= target and arr[i+1] >= target:
+        #         return i+1
+        # return len(arr)
+        
+        
+        # m -- 2
+        
+        l = 0
+        h = len(arr) - 1
+        ans = len(arr)
+        while l <= h:
+            mid = l + (h-l)// 2
+            if arr[mid] >= target:
+                ans = mid
+                h = mid - 1
+            else:
+                l = mid + 1
+        return ans
