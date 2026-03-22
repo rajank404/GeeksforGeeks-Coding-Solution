b5f04423fa23cfa48636ce9b0723965881a18578
Rajan Kumar <101178067+rajank404@users.noreply.github.com>
2026-03-22 22:52:30 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Search in Rotated Array 2/search-in-rotated-array-2.py
+++ Difficulty: Medium/Search in Rotated Array 2/search-in-rotated-array-2.py
@@ -1,49 +1,28 @@
-#User function Template for python3
-
 class Solution:
-    def Search(self, nums, target):
+    def search(self, nums, target):
         # code here
         l = 0
-        r = len(nums) - 1
-        while l <= r:
-            mid = (l+r)//2
+        h = len(nums) - 1
+        while l <= h:
+            mid = l + (h-l)//2
             if nums[mid] == target:
                 return True
-            elif nums[l] == nums[mid] and nums[mid] == nums[r]:
+            
+            elif nums[l] == nums[mid] and nums[mid] == nums[h]:
                 l = l + 1
-                r = r - 1
+                h = h - 1
                 continue
             elif nums[l] <= nums[mid]:
-                if nums[l] <= target and nums[mid] >= target:
-                    r = mid - 1
+                if nums[l] <= target and target <= nums[mid]:
+                    h = mid - 1
                 else:
                     l = mid + 1
             else:
-                if nums[mid] <= target and target <= nums[r]:
+                if nums[mid] <= target and target <= nums[h]:
                     l = mid + 1
                 else:
-                    r = mid - 1
+                    h = mid - 1
         return False
-
-
-#{ 
- # Driver Code Starts
-#Initial Template for Python 3
-#Position this line where user code will be pasted.
-#Initial Template for Python 3
-
-if __name__ == "__main__":
-    t = int(input())
-    while t > 0:
-        k = int(input())
-        arr = list(map(int, input().split()))
-        ob = Solution()
-        res = ob.Search(arr, k)
-        if res:
-            print("true")
-        else:
-            print("false")
-        print("~")
-        t -= 1
-
-# } Driver Code Ends
+            
+        
+        
