ba60e7f4dbdd142ac1b20230ebc392bbaf0b6605
Rajan Kumar <101178067+rajank404@users.noreply.github.com>
2026-03-23 08:59:04 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Find Kth Rotation/find-kth-rotation.py
+++ Difficulty: Easy/Find Kth Rotation/find-kth-rotation.py
@@ -1,28 +1,17 @@
-#User function Template for python3
 class Solution:
-    def findKRotation(self, nums):
+    def findKRotation(self, arr):
         # code here
         l = 0
-        r = len(nums)-1
-        while l < r:
-            mid = (l+r)//2
-            if nums[mid] > nums[r]:
+        h = len(arr) - 1
+        ans = 1000000
+        while l <= h:
+            mid = l + (h-l) // 2 
+            if arr[l] <= arr[mid]:
+                ans = min(ans,arr[l])
                 l = mid + 1
-            else:
-                r = mid
-        return r
-
-
-#{ 
- # Driver Code Starts
-#Initial Template for Python 3
-
-if __name__ == "__main__":
-    t = int(input().strip())
-    for _ in range(t):
-        arr = list(map(int, input().strip().split()))
-        ob = Solution()
-        res = ob.findKRotation(arr)
-        print(res)
-        print("~")
-# } Driver Code Ends
+            elif arr[mid] <= arr[h]:
+                ans = min(ans,arr[mid])
+                h = mid - 1
+        return arr.index(ans)
+        
+        
