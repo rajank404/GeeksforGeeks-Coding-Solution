05929e7bc31b854a22c43eb6f42212f7786ee6f5
Rajan Kumar <101178067+rajank404@users.noreply.github.com>
2026-03-24 20:43:48 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Square Root/square-root.py
+++ Difficulty: Easy/Square Root/square-root.py
@@ -1,49 +1,12 @@
-#User function Template for python3
-
-
-#Complete this function
 class Solution:
-    def floorSqrt(self, x): 
-    #Your code here
-        if x == 0:
-            return 0
-        first = 1
-        last = x
-        while first <= last:
-            mid = first + (last-first)//2
-            result = x // mid
-            if result == mid:
-                return mid
-            elif result < mid:
-                last = mid - 1
-            elif result > mid:
-                first = mid + 1
-        return last
-   
-
-
-
-
-
-#{ 
- # Driver Code Starts
-#Initial Template for Python 3
-
-import math
-
-
-def main():
-    T = int(input())
-    while (T > 0):
-
-        x = int(input())
-
-        print(Solution().floorSqrt(x))
-
-        T -= 1
-
-
-if __name__ == "__main__":
-    main()
-
-# } Driver Code Ends
+    def floorSqrt(self, n): 
+        # code here
+        ans = 1
+        for i in range(1,n):
+            if i*i <= n:
+                ans = i
+            else:
+                break
+        return ans
+                
+        
