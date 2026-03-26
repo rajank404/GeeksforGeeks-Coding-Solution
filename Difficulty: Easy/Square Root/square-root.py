bcad7996e489a97cfa0312da67fa9075dbafd8c1
Rajan Kumar <101178067+rajank404@users.noreply.github.com>
2026-03-26 20:43:37 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Square Root/square-root.py
+++ Difficulty: Easy/Square Root/square-root.py
@@ -1,12 +1,33 @@
 class Solution:
-    def floorSqrt(self, n): 
+    def floorSqrt(self, x): 
         # code here
+        
+        # brute force (Linear Search)
+        
+        
+        # ans = 1
+        # for i in range(1,n):
+        #     if i*i <= n:
+        #         ans = i
+        #     else:
+        #         break
+        # return ans
+        
+        l = 0
+        h = x
         ans = 1
-        for i in range(1,n):
-            if i*i <= n:
-                ans = i
+        while l <= h:
+            mid = l+(h-l)//2
+            temp = mid*mid
+            if temp == x:
+                return mid
+            elif temp < x:
+                l = mid + 1
+                ans = mid
             else:
-                break
+                h = mid - 1
+
         return ans
+        
                 
         
