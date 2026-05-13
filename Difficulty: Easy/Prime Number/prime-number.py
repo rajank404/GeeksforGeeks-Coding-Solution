408862e95c8d1dd3c5a9b0e624736e62fb486f34
Rajan Kumar <101178067+rajank404@users.noreply.github.com>
2026-05-13 08:59:23 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Prime Number/prime-number.py
+++ Difficulty: Easy/Prime Number/prime-number.py
@@ -1,10 +1,11 @@
 class Solution:
     def isPrime(self, n):
         # code here
-        if n < 2:
+        if n == 1:
             return False
-        else:
-            for i in range(2,int(n**0.5)+1):
-                if n % i == 0:
-                    return False
-            return True
+        
+        for i in range(2,int(n**0.5+1)):
+            if n % i == 0:
+                return False
+        return True
+                
