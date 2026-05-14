c626af3db69f4078f6b4a09791ba68f1e8678360
Rajan Kumar <101178067+rajank404@users.noreply.github.com>
2026-05-14 19:04:51 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Factorial/factorial.py
@@ -0,0 +1,9 @@
+class Solution:
+    # Function to calculate factorial of a number.
+    def factorial(self, n: int) -> int:
+        # code here
+        result = 1
+        for i in range(1,n+1):
+            result = result * i
+        return result
+        
