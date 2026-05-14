76bfa29a72d3bbee83b2be71ff0d81cad88315ee
Rajan Kumar <101178067+rajank404@users.noreply.github.com>
2026-05-14 17:24:16 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/GCD of two numbers/gcd-of-two-numbers.py
+++ Difficulty: Basic/GCD of two numbers/gcd-of-two-numbers.py
@@ -2,18 +2,18 @@ import math
 class Solution:
     def gcd(self, a, b):
         # code here
-        # if a > b:
-        #     small = b
-        # else:
+        # result = 1
+        # if a < b:
         #     small = a
-        
+        # else:
+        #     small = b
+            
         # for i in range(1,small+1):
         #     if a % i == 0 and b % i == 0:
-        #         gcd = i
-        # return gcd
+        #         result = i
+        # return result
         
         if b == 0:
             return a
         else:
             return math.gcd(b,a%b)
-            
