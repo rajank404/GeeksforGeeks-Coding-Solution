56b1d50470cd0abaed726eaedf1912b44c3c9280
Rajan Kumar <101178067+rajank404@users.noreply.github.com>
2026-05-18 21:00:38 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Ceil in a Sorted Array/ceil-in-a-sorted-array.py
+++ Difficulty: Easy/Ceil in a Sorted Array/ceil-in-a-sorted-array.py
@@ -1,16 +1,18 @@
 #User function Template for python3
 class Solution:
-    def findCeil(self, arr, target):
+    def findCeil(self, arr, x):
         # code here
         l = 0
-        h = len(arr) - 1
         ans = -1
-  
+       
+        h = len(arr) - 1
         while l <= h:
-            mid = l + (h-l) // 2
-            if arr[mid] >= target:
+            mid = l+ (h-l)//2
+            if arr[mid] >= x:
                 ans = mid
                 h = mid - 1
             else:
                 l = mid + 1
         return ans
+        
+    
